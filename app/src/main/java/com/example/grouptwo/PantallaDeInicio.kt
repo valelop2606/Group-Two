26c9fe77faf3bd4c675e694f09a6b9bd5bfd3843
valelop2606 <valeriuslopezpinto@gmail.com>
2025-10-16 20:51:48 -0400

    Correcciones para que la pantalla inicial compile, aun no es funcional pero puede correr en emulador

---
+++ app/src/main/java/com/example/grouptwo/PantallaDeInicio.kt
@@ -0,0 +1,11 @@
+package com.example.grouptwo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaDeInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_de_inicio) // <-- este SÍ es tu layout
+    }
+}
--- app/src/main/java/pantalla_de_inicio.kt
+++
@@ -1,2 +0,0 @@
-class pantalla_de_inicio {
-}
