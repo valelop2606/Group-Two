75144877c32b47887d48131f348242bea9623476
valelop2606 <valeriuslopezpinto@gmail.com>
2025-10-22 21:32:13 -0400

    Inicios en la pantalla para ver a detalle un coctel al presionarlo

--- app/src/main/java/com/example/grouptwo/PantallaDeInicio.kt
+++ app/src/main/java/com/example/grouptwo/PantallaDeInicio.kt
@@ -1,11 +1,20 @@
 package com.example.grouptwo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class PantallaDeInicio : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.pantalla_de_inicio) // <-- este SÍ es tu layout
+        setContentView(R.layout.pantalla_de_inicio) // <-- tu xml de arriba
+
+        val imgMojito: ImageView = findViewById(R.id.imgMojito)
+        imgMojito.setOnClickListener {
+            val i = Intent(this, VerRecetaDetallada::class.java)
+            startActivity(i)
+        }
     }
 }
