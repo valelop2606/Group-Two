ea6ed6ca04fe2ca055587b808cc2c4404f6cfdd1
valelop2606 <valeriuslopezpinto@gmail.com>
2025-10-23 11:03:14 -0400

    Agregar los incios en las pantallas para ver las recetas de los cocteles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.grouptwo"
     compileSdk = 36
 
+    buildFeatures { viewBinding = true }
+    defaultConfig { vectorDrawables.useSupportLibrary = true }
+
+
     defaultConfig {
         applicationId = "com.example.grouptwo"
         minSdk = 29
@@ -48,4 +52,9 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
+
 }
