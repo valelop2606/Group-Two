3fb4e8f04320e3f599b191f4f637a857dd225049
MATIAS <matiassebastiansc113@gmail.com>
2025-10-24 09:28:01 -0400

    Log in creado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/grouptwo/InicioSesionActivity.kt
+++ app/src/main/java/com/example/grouptwo/InicioSesionActivity.kt
@@ -1,7 +1,9 @@
 package com.example.grouptwo
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,10 +11,16 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.grouptwo.databinding.ActivityCategoriasBinding
 import com.example.grouptwo.databinding.ActivityInicioSesionBinding
 import com.example.grouptwo.databinding.ActivityPantallaInicialBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
 
 class InicioSesionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityInicioSesionBinding
+    private lateinit var auth: FirebaseAuth
+    val context: Context = this
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +35,84 @@ class InicioSesionActivity : AppCompatActivity() {
             insets
         }
 
+        auth = Firebase.auth
+
         binding.botonInicioSesion.setOnClickListener {
-            val intent = Intent(this, PantallaInicialActivity::class.java)
-            startActivity(intent)
+            val correo = binding.TextoUsername.text.toString()
+            val pass = binding.TextoPassword.text.toString()
+            if(correo.isNotEmpty() && pass.isNotEmpty()){
+                loginValidation(correo, pass)
+            } else {
+                Toast.makeText(
+                    baseContext,
+                    "Debe ingresar correo y contraseña.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+
+        binding.botonCrearCuenta.setOnClickListener {
+            val correo = binding.TextoUsername.text.toString()
+            val pass = binding.TextoPassword.text.toString()
+            if(pass.length < 8 && correo.isNotEmpty()){
+                Toast.makeText(
+                    baseContext,
+                    "Debe ingresar una contraseña de 8 o más dígitos.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                crearUsuario(correo, pass)
+            }
+
+        }
+
+    }
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intentPantallaPrincipal = Intent(context, PantallaInicialActivity::class.java)
+            startActivity(intentPantallaPrincipal)
         }
     }
+
+    fun crearUsuario(
+        correo: String,
+        pass:String
+    ){
+        auth.createUserWithEmailAndPassword(correo, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intentLogin =
+                        Intent(context, InicioSesionActivity::class.java)
+                    startActivity(intentLogin)
+
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    fun loginValidation(
+        correo: String,
+        pass: String
+    ) {
+        auth.signInWithEmailAndPassword(correo, pass).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                // El usuario se logueó correctamente
+            } else {
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+    }
+
+
 }
